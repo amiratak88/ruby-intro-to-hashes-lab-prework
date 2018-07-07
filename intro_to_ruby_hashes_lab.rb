0d98f995309602663421556de8e512e665229daf
Amirata K <amiratak88@gmail.com>
2018-07-07 05:36:11 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,9 @@ def monopoly_with_third_tier
   monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
   monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
   monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
-  monopoly[:railroads][:names][]
+  monopoly[:railroads][:names][:pennsylvania_railroad] = {}
+  monopoly[:railroads][:names][:pennsylvania_railroad] = {}
+
 end
 
 def monopoly_with_fourth_tier
